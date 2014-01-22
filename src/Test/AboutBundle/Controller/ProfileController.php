62d7c5a20d342b80c8dc7ff3dc4e9466dc4382cb
Lindsey Harding <lharding@uga.edu>
2014-01-22 10:33:01 -0500

    controller tweak

--- src/Test/AboutBundle/Controller/ProfileController.php
+++ src/Test/AboutBundle/Controller/ProfileController.php
@@ -53,7 +53,7 @@ class ProfileController extends Controller
             $em->persist($entity);
             $em->flush();
 
-            return $this->redirect($this->generateUrl('profile_show', array('id' => $entity->getId())));
+            return $this->redirect($this->generateUrl('profile', array('id' => $entity->getId())));
         }
 
         return array(
